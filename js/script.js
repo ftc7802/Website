51c4c9d40ca2f697f2359407ebecb11839734695
ftc7802 <YOUR EMAIL ADDRESS>
2015-10-20 18:11:24 -0500

    Everything

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//JAVASCRIPTS FOUND AND COMPILED TOGETHER IN ONE FILE SECTIONED OFF
+
+//SCROLLIT
+
+function scrollit(seed) {
+var m1=""
+var msg=m1;
+var out= " ";
+var c= 1;
+if (seed > 100) {
+seed--;
+cmd="scrollit("+seed+")";
+timerTwo=window.setTimeout(cmd,100);
+}
+else if (seed <= 100 && seed > 0) {
+for (c=0 ; c < seed ; c++) {
+out+=" ";
+}
+out+=msg;
+seed--;
+window.status=out;
+cmd="scrollit("+seed+")";
+timerTwo=window.setTimeout(cmd,100);
+}
+else if (seed <= 0) {
+if (-seed < msg.length) {
+out+=msg.substring(-seed,msg.length);
+seed--;
+window.status=out;
+cmd="scrollit("+seed+")";
+timerTwo=window.setTimeout(cmd,100);
+}
+else {
+window.status=" ";
+timerTwo=window.setTimeout("scrollit(100)",75);
+      }
+   }
+}
+
+//////////////////////////////////////////////////
+
+//
